402e896a83e28634358600342394e5c81bd9baa4
justin <justin@BIGSHELL>
2016-08-07 04:09:36 -0400

    built view all tasks and create tasks but both are broken: view doesn't show any tasks and create only half creates tasks.

---
+++ Week8WeekendHomework/Week8WeekendHomework/Controllers/TaskTODOController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Week8WeekendHomework.Models;
+
+namespace Week8WeekendHomework.Controllers
+{
+    public class TaskTODOController : Controller
+    {
+        taskdbmodelDataContext taskdb = new taskdbmodelDataContext();
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult GetTaskList()
+        {
+            var usr = User.Identity.Name;
+
+            var myTasks = TaskTableHomework.GetMyTasks(taskdb, usr);
+
+            return View(myTasks);
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult CreateTask()
+        {
+
+            return View();
+        }
+        
+        [HttpPost]
+        [Authorize]
+        public ActionResult CreateTask([Bind(Include = "TaskName")] TaskTableHomework task)
+        {
+            TaskTableHomework.CreateTask(task, User.Identity.Name, taskdb);
+
+            return RedirectToAction("GetTaskList");
+
+        }
+    }
+}
---
+++ Week8WeekendHomework/Week8WeekendHomework/Models/TaskTableHomework.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Week8WeekendHomework.Models
+{
+    public partial class TaskTableHomework
+    {
+        public static IEnumerable<TaskTableHomework> GetMyTasks(taskdbmodelDataContext taskdb, string userName)
+        {
+            var myTasks = from tskUsr in taskdb.TaskUserLogins
+                           join tsk in taskdb.TaskTableHomeworks on tskUsr.TaskID equals tsk.TaskID
+                           where tskUsr.UserName == userName
+                           select tsk;
+
+            return myTasks;
+        }
+
+        public static void CreateTask(TaskTableHomework task, string usrName, taskdbmodelDataContext taskdb)
+        {
+            taskdb.TaskTableHomeworks.InsertOnSubmit(task);
+            taskdb.SubmitChanges();
+
+            var taskID = (from t in taskdb.TaskTableHomeworks
+                          where t.TaskName == task.TaskName
+                          select t.TaskID).FirstOrDefault();
+
+            TaskUserLogin tskUser = new TaskUserLogin();
+
+            tskUser.UserName = usrName;
+            tskUser.TaskID = taskID;
+
+            taskdb.TaskUserLogins.InsertOnSubmit(tskUser);
+            taskdb.SubmitChanges();
+        }
+    }
+}
