2f7f1fec8e3306b54bb793dccbb9e32eae40712a
justin <justin@BIGSHELL>
2016-08-07 21:44:01 -0400

    all done

--- Week8WeekendHomework/Week8WeekendHomework/Controllers/TaskTODOController.cs
+++ Week8WeekendHomework/Week8WeekendHomework/Controllers/TaskTODOController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using Week8WeekendHomework.Models;
@@ -12,13 +14,13 @@ namespace Week8WeekendHomework.Controllers
         taskdbmodelDataContext taskdb = new taskdbmodelDataContext();
 
 
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+
         public ActionResult Index()
-        {
-            return View();
-        }
-
-
-        public ActionResult GetTaskList()
         {
             var usr = User.Identity.Name;
 
@@ -41,8 +43,75 @@ namespace Week8WeekendHomework.Controllers
         {
             TaskTableHomework.CreateTask(task, User.Identity.Name, taskdb);
 
-            return RedirectToAction("GetTaskList");
+            return RedirectToAction("Index");
 
         }
+
+        [Authorize]
+        public ActionResult EditTask(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+            TaskTableHomework tsk = TaskTableHomework.GetTaskById(id, taskdb);
+
+            return View(tsk);
+        }
+
+        [Authorize]
+        [HttpPost]
+        public ActionResult EditTask([Bind(Include = "TaskID, TaskName, TaskDescription, TaskDueDate, TaskComplete")] TaskTableHomework task)
+        {
+            TaskTableHomework.EditTask(task, taskdb);
+
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        public ActionResult DeleteTask(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.NotFound);
+
+            TaskTableHomework task = TaskTableHomework.GetTaskById(id, taskdb);
+
+            return View(task);
+        }
+
+        [Authorize]
+        [HttpPost, ActionName("DeleteTask")]
+        public ActionResult DeleteActual(int? id)
+        {
+            TaskTableHomework.DeleteTaskByID(id, taskdb);
+
+            return RedirectToAction("Index");
+        }
+
+        [Authorize]
+        public ActionResult getTasksDueTodayC(int? id)
+        {
+            TaskTableHomework instance = new TaskTableHomework();
+            return View("~/Views/TaskTODO/Index.cshtml", instance.getTasksDueToday(id, taskdb));
+        }
+
+        public ActionResult getTasksDueTomorrowC(int? id)
+        {
+            TaskTableHomework instance = new TaskTableHomework();
+            return View("~/Views/TaskTODO/Index.cshtml", instance.getTasksDueTomorrow(id, taskdb));
+        }
+
+        public ActionResult getTasksCompletedC()
+        {
+            TaskTableHomework instance = new TaskTableHomework();
+            return View("~/Views/TaskTODO/Index.cshtml", instance.getTasksCompleted(taskdb));
+        }
+
+        public ActionResult getTasksOverDueC()
+        {
+            TaskTableHomework instance = new TaskTableHomework();
+            return View("~/Views/TaskTODO/Index.cshtml", instance.getTasksOverDue(taskdb));
+        }
     }
+
+    
 }
--- Week8WeekendHomework/Week8WeekendHomework/Models/TaskTableHomework.cs
+++ Week8WeekendHomework/Week8WeekendHomework/Models/TaskTableHomework.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -7,12 +9,25 @@ namespace Week8WeekendHomework.Models
 {
     public partial class TaskTableHomework
     {
+        [DisplayFormat(DataFormatString = "{0:MM/dd/yyyy}")]
+        public DateTime? FormattedDate
+        {
+            get
+            {
+                return Convert.ToDateTime(this.TaskDueDate);
+            }
+            set
+            {
+                this.TaskDueDate = value;
+            }
+        }
+
         public static IEnumerable<TaskTableHomework> GetMyTasks(taskdbmodelDataContext taskdb, string userName)
         {
             var myTasks = from tskUsr in taskdb.TaskUserLogins
-                           join tsk in taskdb.TaskTableHomeworks on tskUsr.TaskID equals tsk.TaskID
-                           where tskUsr.UserName == userName
-                           select tsk;
+                          join tsk in taskdb.TaskTableHomeworks on tskUsr.TaskID equals tsk.TaskID
+                          where tskUsr.UserName == userName
+                          select tsk;
 
             return myTasks;
         }
@@ -33,6 +48,88 @@ namespace Week8WeekendHomework.Models
 
             taskdb.TaskUserLogins.InsertOnSubmit(tskUser);
             taskdb.SubmitChanges();
+        }
+
+        public static void EditTask(TaskTableHomework task, taskdbmodelDataContext taskdb)
+        {
+            var orgTask = (from t in taskdb.TaskTableHomeworks
+                           where t.TaskID == task.TaskID
+                           select t).FirstOrDefault();
+
+            orgTask.TaskName = task.TaskName;
+            orgTask.TaskDescription = task.TaskDescription;
+            orgTask.TaskDueDate = task.TaskDueDate;
+            orgTask.TaskComplete = task.TaskComplete;
+            taskdb.SubmitChanges();
+        }
+
+        public static TaskTableHomework GetTaskById(int? id, taskdbmodelDataContext taskdb)
+        {
+            TaskTableHomework tsk = (from t in taskdb.TaskTableHomeworks
+                                     where t.TaskID == id
+                                     select t).FirstOrDefault();
+
+            return tsk;
+        }
+
+        public static void DeleteTaskByID(int? id, taskdbmodelDataContext taskdb)
+        {
+            var TaskUsersWithSameId = from tu in taskdb.TaskUserLogins
+                                      where tu.TaskID == id
+                                      select tu;
+
+            taskdb.TaskUserLogins.DeleteAllOnSubmit(TaskUsersWithSameId);
+            taskdb.SubmitChanges();
+
+
+            TaskTableHomework task = (from t in taskdb.TaskTableHomeworks
+                                      where t.TaskID == id
+                                      select t).FirstOrDefault();
+
+            taskdb.TaskTableHomeworks.DeleteOnSubmit(task);
+            taskdb.SubmitChanges();
+        }
+
+        public List<TaskTableHomework> getTasksDueToday(int? id, taskdbmodelDataContext taskdb)
+        { // compare all tasks' .duedates and return a list of the ones due today
+
+            var TasksDueToday = (from tdd in taskdb.TaskTableHomeworks
+                                 where tdd.TaskDueDate == DateTime.Now.Date
+                                 select tdd).ToList();
+            return TasksDueToday;
+        }
+
+        public List<TaskTableHomework> getTasksDueTomorrow(int? id, taskdbmodelDataContext taskdb)
+        { // compare all tasks' .duedates and return a list of the ones due tomorrow
+            DateTime DateTimeVar = DateTime.Now.AddDays(1);
+
+            var TasksDueTomorrow = (from tdd in taskdb.TaskTableHomeworks
+                                    where tdd.TaskDueDate == DateTimeVar
+                                    select tdd).ToList();
+            return TasksDueTomorrow;
+        }
+
+        public List<TaskTableHomework> getTasksCompleted(taskdbmodelDataContext taskdb)
+        { // compare all tasks' completed bit and return a list of the ones set to 1
+
+            List<TaskTableHomework> returnList = (from v in taskdb.TaskTableHomeworks
+                                                  where v.TaskComplete == true
+                                                  select v).ToList();
+            return returnList;
+        }
+
+        public List<TaskTableHomework> getTasksOverDue(taskdbmodelDataContext taskdb)
+        { // compare all tasks' completed bit and return a list of the ones set to 1
+
+            //List<TaskTableHomework> returnList = (from v in taskdb.TaskTableHomeworks
+            //                                      where DbFunctions.TruncateTime(v.TaskDueDate) < DbFunctions.TruncateTime(DateTime.Now) && v.TaskComplete != true
+            //                                      select v).ToList();
+
+            List<TaskTableHomework> returnList = (from v in taskdb.TaskTableHomeworks
+                                                   where v.TaskDueDate < DateTime.Now.Date && v.TaskComplete != true
+                                                   select v).ToList();
+
+            return returnList;
         }
     }
 }
